2301d51a8bfde4faca4c1cf75a2327b40c14744c
maxime barber <maxime.b03@outlook.fr>
2022-01-10 17:23:17 +0100

    tout a disparue

--- Project GIT/romain.cpp
+++ Project GIT/romain.cpp
@@ -1,17 +1,4 @@
 #include<iostream>
 using namespace std;
 
-int main() {
-	
-	char c[20];
-	cin >> c;
-
-	int a = 0;
-
-	for (int i = 0; i < strlen(c); i++)
-		if (c[i] == 'a')
-			a++;
-	int kk;
-
-	return 0;
-}
+// tout a disparue
